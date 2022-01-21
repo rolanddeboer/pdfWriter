3629ec1f3cac68deda792962c9e42d543fecf47e
Roland de Boer <roland@rolanddeboer.com>
2022-01-21 20:09:33 +0100

    .

--- lib/hummus/build/ZLib/CMakeFiles/Zlib.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libZlib.pdb"
-  "libZlib.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C)
-  include(CMakeFiles/Zlib.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
